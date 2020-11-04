3a6f4a6fa2d8d148c98e4588fbb9eb04ac73d1bc
Curtis Sapp <curtmsapp@gmail.com>
2020-11-03 17:47:59 -0700

    Added function stubs

---
+++ src/FileUtils.py
@@ -0,0 +1,6 @@
+def getMessageFromFile(index):
+    return "The message at this index"
+
+
+def getMessageCount():
+    return "Message Count (its a number)"
--- src/main.py
+++ src/main.py
@@ -1,2 +1,4 @@
 # Main File
 
+def getDailyMessage():
+    return "The message using file utils and some random number"
